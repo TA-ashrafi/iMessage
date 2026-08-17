d3015873748d6780622e593b3588dd08dd0de213
TA-ashrafi <tahseenashrafi29@gmail.com>
2026-08-17 17:07:38 +0530

    Server Done

---
+++ backend/src/index.js
@@ -0,0 +1,5 @@
+import express from "express"
+import "dotenv/config"
+const app = express()
+const PORT = process.env.PORT
+app.listen(PORT, () => console.log("Server is UP and running on PORT :",PORT));
